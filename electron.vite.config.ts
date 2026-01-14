4bab965f3c1999559bd4942610f845aecab69231
KierenDayStudios <kddevelopmentofficial@gmail.com>
2026-01-14 00:46:11 +0000

    a

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   main: {
     build: {
-      outDir: 'out/main'
+      outDir: 'out/main',
+      rollupOptions: {
+        external: ['electron', 'electron-updater']
+      }
     }
   },
   preload: {
